cc93a155563f1b8b586582b99d4659b0becc3839
Enrico Gaffo <enrico.gaffo@gmail.com>
2023-05-08 14:18:06 +0200

    just some code linting

--- R/combine_ccp2_runs.R
+++ R/combine_ccp2_runs.R
@@ -1,9 +1,18 @@
-#' Combine results of different CirComPara2 runs
+#' Combine the results of multiple CirComPara2 analyses
+#'
+#' This function will combine multiple CirComPara2 analyses into single output
+#' result files. Specifically, it combines the circRNA expression of all 
+#' samples from the different projects into one expression matrix reporting the 
+#' backsplice junction read counts. Moreover, it also combines the read counts 
+#' of the reads linearly spliced on the backsplice junctions. For the circRNAs 
+#' not detected in all the projects it will calculate the linearly spliced read 
+#' counts by parsing the linear alignment files.
 #'
 #' @param files a character indicating the path of the parent directory of the
 #' CirComPara2 runs to be combined.
-#' \code{files} child directories will be scan to search for the CirComPara2 results
-#' to merge (i.e. the circRNA backsplice read counts and linear read counts).
+#' \code{files} child directories will be scan to search for the CirComPara2 
+#' results to merge (i.e. the circRNA backsplice read counts and linear read 
+#' counts).
 #' \code{files} might also be a file listing either (1) directories,
 #' (2) bks.counts.union.csv files, or (3) a mix of directories and files.
 #' @param min_methods the minimum number of circRNA-detection methods a circRNA
@@ -17,7 +26,7 @@
 #' previously computed backsplice linear alignment read count files and only
 #' compute the possible missed backsplices that have been detected only in other
 #' samples. FALSE by default. (this option is not currently implemented yet).
-#' @param isPairedEnd logical indicating if the linear alignments are paired-end.
+#' @param is_paired_end logical indicating if the linear alignments are paired-end.
 #' TRUE by default.
 #' @param cpus integer indicating the number of parallel threads to use. 1 by
 #' default.
@@ -35,28 +44,28 @@ combine_ccp2_runs <-
   function(files,
            min_methods = 2,
            min_reads = 2,
-           merge_lin = F,
-           recycle_existing_lincount = F,
-           isPairedEnd = T,
+           merge_lin = FALSE,
+           recycle_existing_lincount = FALSE,
+           is_paired_end = TRUE,
            cpus = 1) {
 
     # files <- "/sharedfs01/circrna/zicirc/analysis/GSE159225/batch"
     # require(data.table)
     # require(Rsubread)
 
-    if(dir.exists(files)) {
-      ## if files is a directory, search all subdirectories for the CCP2 results
-      ## to merge
+    if (dir.exists(files)) {
+      #' if files is a directory, search all sub directories for the CCP2
+      #' results to merge
 
       ccp_count_files <-
         dir(path = files,
             pattern = "bks.counts.union.csv",
-            full.names = T,
-            recursive = T)
-    }else{
-      ## files might be a file listing either
-      ## 1) directories to merge,
-      ## 2) bks.counts.union.csv files to merge, or
+            full.names = TRUE,
+            recursive = TRUE)
+    }else {
+      ## 'files' might be one file listing either
+      ## 1) the CCP2 project directories to merge,
+      ## 2) the 'bks.counts.union.csv' files to merge, or
       ## 3) a mix of directories and files
 
       files <- readLines(files)
@@ -67,22 +76,31 @@ combine_ccp2_runs <-
                  f,
                  dir(path = files_dir,
                      pattern = "bks.counts.union.csv",
-                     full.names = T,
-                     recursive = T))
+                     full.names = TRUE,
+                     recursive = TRUE))
         })
     }
 
-    ccp_counts <- rbindlist(sapply(ccp_count_files,
-                                   fread,
-                                   simplify = F),
-                            use.names = T)
+    message("Combining BJR counts from ", length(ccp_count_files),
+            "projects...")
+    ccp_counts <- data.table::rbindlist(sapply(ccp_count_files,
+                                               data.table::fread,
+                                               simplify = FALSE),
+                                        use.names = TRUE)
 
     ccp_counts[, circ_id := paste0(chr, ":", start, "-", end)]
+    message("Detected ", nrow(ccp_counts), " circRNAs")
 
+    message("Removing circRNAs detected with <= ",
+            min_reads,
+            " BJRs by <= ",
+            min_methods,
+            " circRNA detection methods, in all samples...")
     reliable_circ_ids <-
       ccp_counts[n_methods >= min_methods &
                    read.count >= min_reads,
                  unique(circ_id)]
+    message(length(reliable_circ_ids), " reliable circRNAs were kept.")
 
     ccp_counts_dt <-
       dcast(data = ccp_counts[circ_id %in% reliable_circ_ids],
@@ -92,16 +110,19 @@ combine_ccp2_runs <-
 
     lin_bks_counts <- NA
 
-    if(merge_lin) {
+    if (merge_lin) {
       ## merge also the linear counts
       ## N.B. this might require to compute the linear counts for circRNAs not
       ## detected in the sample (but detected in other samples)
+      message("Merging the linearly spliced read counts...")
 
-      if(recycle_existing_lincount) {
+      if (recycle_existing_lincount) {
         ## recycle the existing lincount files and compute the lincounts
         ## for the circRNAs expressed only in other samples
         ## TODO
-      }else{
+        message("Use of pre-computed linearly spliced read counts ",
+                "not yet implemented. Skipping.")
+      }else {
         ## count the linearly spliced reads on the backsplice ends
 
         ## retrieve HISAT2 BAM files from the CCP2 project directories
@@ -113,24 +134,31 @@ combine_ccp2_runs <-
         ccp2_runs_basedirs <- unique(sub(ccp_count_file_postfix,
                                          "",
                                          ccp_count_files))
+        message("Counting the reads linearly spliced on the backsplice ",
+                "junctions from ",
+                length(ccp2_runs_basedirs),
+                " projects...")
 
         bam_files <-
           unlist(sapply(ccp2_runs_basedirs,
-                        FUN = function(f){dir(path = f,
-                                              pattern = "_hisat2.bam$",
-                                              full.names = T,
-                                              recursive = T)},
-                        simplify = T,
-                        USE.NAMES = F))
+                        FUN = function(f) {
+                          dir(path = f,
+                              pattern = "_hisat2.bam$",
+                              full.names = TRUE,
+                              recursive = TRUE) },
+                        simplify = TRUE,
+                        USE.NAMES = FALSE))
 
         names(bam_files) <-
           sapply(bam_files,
-                 function(f)sub("_hisat2.bam", "", basename(f)),
-                 USE.NAMES = F)
+                 function(f)sub( "_hisat2.bam", "", basename(f) ),
+                 USE.NAMES = FALSE)
+        message("Parsing ", length(bam_files), " linear alignment files...")
 
         ## make SAF data.frame of start/end of circRNAs
+        ## TODO: strandedness
         sn_unique_circ <-
-          melt(ccp_counts_dt[, tstrsplit(circ_id, ":|-", type.convert = F),
+          melt(ccp_counts_dt[, tstrsplit(circ_id, ":|-", type.convert = FALSE),
                              by = circ_id],
                id.vars = c("circ_id", "V1"),
                variable.name = "featureType",
@@ -149,32 +177,31 @@ combine_ccp2_runs <-
                                                          featureType)])
 
         ## count spliced reads on circRNA junctions gene expression
-        ## usign the featureCounts function of the Rsubread package
+        ## using the featureCounts function of the Rsubread package
         fc_linear_backsplices <-
           featureCounts(files = bam_files,
                         annot.ext = circ_ids_saf,
-                        splitOnly = T,
-                        nonSplitOnly = F,
-                        useMetaFeatures = T,
-                        allowMultiOverlap = T,
-                        countMultiMappingReads = T,
-                        countReadPairs = T,
-                        countChimericFragments = F,
-                        largestOverlap = T,
-                        primaryOnly = T,
+                        splitOnly = TRUE,
+                        nonSplitOnly = FALSE,
+                        useMetaFeatures = TRUE,
+                        allowMultiOverlap = TRUE,
+                        countMultiMappingReads = TRUE,
+                        countReadPairs = TRUE,
+                        countChimericFragments = FALSE,
+                        largestOverlap = TRUE,
+                        primaryOnly = TRUE,
                         strandSpecific = 0,
-                        isPairedEnd = isPairedEnd,
+                        isPairedEnd = is_paired_end,
                         nthreads = cpus)
 
         lin_bks_counts <-
           data.table(fc_linear_backsplices$counts,
                      keep.rownames = "circ_id")
-        colnames(lin_bks_counts) <- sub("_hisat2.bam", "", colnames(lin_bks_counts))
+        colnames(lin_bks_counts) <- sub("_hisat2.bam", "",
+                                        colnames(lin_bks_counts))
       }
     }
 
     list(circ_read_count_mt = ccp_counts_dt,
          lin_read_count_mt = lin_bks_counts)
-
   }
-
