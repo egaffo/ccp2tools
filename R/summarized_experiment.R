22fc3f1d135b9ee70e8b606172cb6faea41055a8
Enrico Gaffo <enrico.gaffo@gmail.com>
2025-12-04 12:23:24 +0100

    improved docs; implemented extra coldata attachment; included metadata from tximport object

--- R/summarized_experiment.R
+++ R/summarized_experiment.R
@@ -1,11 +1,13 @@
-#' Convert the result of combine_ccp2_runs() into a SummarizedExperiment object
+#' Convert the result of [combine_ccp2_runs] into a SummarizedExperiment object
 #'
 #' @param combined_prjs a named list, as resulting from the
-#'  \link[combine_ccp2_runs]{ccp2tools::combine_ccp2_runs()} function. Must include the following entries:
+#'  [combine_ccp2_runs] function. Must include the following entries:
 #'  circ_read_count_mt, lin_read_count_mt, circ_gene_anno, lin_xpr, read_stats,
 #'  gene_anno.
-#' @param assays select which RNA assay to extract among "circrna", "gene", or
+#' @param assay select which RNA assay to extract among "circrna", "gene", or
 #'  "trx" for circrna-, gene-, or transcript-level expression analysis.
+#' @param coldata a data.frame with additional sample data to include into the
+#'  SummarizedExperiment
 #'
 #' @returns a \link{SummarizedExperiment} object for circrna assay; a
 #'  \link{RangedSummarizedExperiment} if gene or transcript assay
@@ -34,107 +36,152 @@
 #'
 to_summarized_experiment <-
   function(combined_prjs,
-           assay = c("circrna", "gene", "trx")) {
+           assay = c("circrna", "gene", "trx"),
+           coldata = NULL) {
+    assay <- match.arg(assay)
 
-  assay <- match.arg(assay)
+    sample_metadata <- data.frame(combined_prjs$read_stats,
+                                  row.names = "Sample",
+                                  check.names = F)
 
-  sample_metadata <- data.frame(combined_prjs$read_stats,
-                                row.names = "Sample",
-                                check.names = F)
+    # ---- circRNA-level ----
+    if (assay == "circrna") {
+      ## get one annotation line per circRNA id
+      circrna_hosts <-
+        unique(combined_prjs$circ_gene_anno[, .(circ_id, gene_id, gene_name)])[, lapply(.SD, function(x) {
+          paste0(x, collapse = "|")
+        }), by = circ_id]
 
-  # ---- circRNA-level ----
-  if (assay == "circrna") {
+      intergenic_circs <-
+        combined_prjs$circ_read_count_mt$circ_id[!combined_prjs$circ_read_count_mt$circ_id %in% circrna_hosts$circ_id]
 
-    ## get one annotation line per circRNA id
-    circrna_hosts <-
-      unique(combined_prjs$circ_gene_anno[, .(circ_id, gene_id, gene_name)])[, lapply(.SD, function(x) {
-        paste0(x, collapse = "|")
-      }), by = circ_id]
+      circrna_hosts <- merge(
+        circrna_hosts,
+        data.table::data.table(circ_id = intergenic_circs),
+        by = "circ_id",
+        all = T
+      )
 
-    intergenic_circs <-
-      combined_prjs$circ_read_count_mt$circ_id[!combined_prjs$circ_read_count_mt$circ_id %in% circrna_hosts$circ_id]
+      ## the backspliced read counts
+      count_data <- data.frame(
+        combined_prjs$circ_read_count_mt,
+        row.names = "circ_id",
+        check.names = F
+      )
 
-    circrna_hosts <- merge(
-      circrna_hosts,
-      data.table::data.table(circ_id = intergenic_circs),
-      by = "circ_id",
-      all = T
-    )
+      ## linearly-spliced read counts on the backsplices
+      lin_count_data <- data.frame(
+        combined_prjs$lin_read_count_mt,
+        row.names = "circ_id",
+        check.names = F
+      )
 
-    ## the backspliced read counts
-    count_data <- data.frame(
-      combined_prjs$circ_read_count_mt,
-      row.names = "circ_id",
-      check.names = F
-    )
+      ## gene annotation for the circRNAs
+      gene_metadata <- data.frame(circrna_hosts,
+                                  row.names = "circ_id",
+                                  check.names = F)
 
-    ## linearly-spliced read counts on the backsplices
-    lin_count_data <- data.frame(
-      combined_prjs$lin_read_count_mt,
-      row.names = "circ_id",
-      check.names = F
-    )
+      se <- SummarizedExperiment::SummarizedExperiment(
+        assays = list(counts = count_data, linCounts = lin_count_data),
+        colData = sample_metadata,
+        rowData = gene_metadata
+      )
+    }
 
-    ## gene annotation for the circRNAs
-    gene_metadata <- data.frame(circrna_hosts,
-                                row.names = "circ_id",
-                                check.names = F)
+    # ---- gene-level ----
+    if (assay == "gene") {
+      gnx <- tximport::summarizeToGene(combined_prjs$lin_xpr$txi,
+                                       combined_prjs$lin_xpr$tx2gene)
 
-    se <- SummarizedExperiment::SummarizedExperiment(
-      assays = list(counts = count_data, linCounts = lin_count_data),
-      colData = sample_metadata,
-      rowData = gene_metadata
+      rowRanges <- combined_prjs$gene_anno[combined_prjs$gene_anno$type == "gene", ]
+      rowRanges <- rowRanges[rowRanges$gene_id %in% rownames(gnx$counts), ]
+      rowRanges_order <- match(rownames(gnx$counts), GenomicRanges::mcols(rowRanges)$gene_id)
+
+      se <- SummarizedExperiment::SummarizedExperiment(
+        assays = list(
+          counts = gnx$counts,
+          abundance = gnx$abundance,
+          length = gnx$length
+        ),
+        colData = sample_metadata[, c("Raw reads",
+                                      "Clean reads",
+                                      "Dropped",
+                                      "Linearly unmapped",
+                                      "Linearly mapped")],
+        rowRanges = rowRanges[rowRanges_order, ],
+        metadata = list(countsFromAbundance = gnx$countsFromAbundance)
+      )
+
+    }
+
+    # ---- transcript-level ----
+    if (assay == "trx") {
+      rowRanges <- combined_prjs$gene_anno[combined_prjs$gene_anno$type == "transcript", ]
+      rowRanges <- rowRanges[rowRanges$transcript_id %in% rownames(combined_prjs$lin_xpr$txi$counts), ]
+      rowRanges_order <- match(rownames(combined_prjs$lin_xpr$txi$counts),
+                               mcols(rowRanges)$transcript_id)
+
+      se <- SummarizedExperiment::SummarizedExperiment(
+        assays = list(
+          counts = combined_prjs$lin_xpr$txi$counts,
+          abundance = combined_prjs$lin_xpr$txi$abundance,
+          length = combined_prjs$lin_xpr$txi$length
+        ),
+        colData = sample_metadata[, c("Raw reads",
+                                      "Clean reads",
+                                      "Dropped",
+                                      "Linearly unmapped",
+                                      "Linearly mapped")],
+        # rowData = data.frame(combined_prjs$lin_xpr$tx2gene, row.names = "t_name"),
+        rowRanges = rowRanges[rowRanges_order, ],
+        metadata = list(countsFromAbundance = combined_prjs$lin_xpr$txi$countsFromAbundance)
+      )
+
+    }
+
+    if (!is.null(coldata)) {
+      colData(se) <- attach_coldata(colData(se), coldata)
+    }
+
+    return(se)
+  }
+
+
+#' Attach columns to existing data.frame and checks whether row and column names
+#' are consistent.
+#'
+#' @param se_coldata the original data.frame to extend
+#' @param extra_coldata a data.frame with the additional columns to add. Row
+#' and column names must be consistent with the original data.frame, i.e., the
+#' extra_coldata must include all row names and none colnames of the original
+#' data.frame
+#'
+#' @returns a data.frame with the extra columns attached
+#' @export
+#'
+#' @examples \dontrun{
+#' extra_cols <-
+#'   data.frame(condition = rep_len("A", rownames(colData(se))),
+#'              row.names = rownames(colData(se)))
+#' colData(se) <- attach_coldata(colData(se), extra_cols)
+#' }
+attach_coldata <- function(se_coldata, extra_coldata) {
+  ## check all samples are included into the extra coldata
+  if (!all(rownames(se_coldata) %in% rownames(extra_coldata))) {
+    stop(
+      "The extra_coldata does not include all row names of the original data.frame. ",
+      paste("Missing", rownames(se_coldata)[!rownames(se_coldata) %in% rownames(extra_coldata)])
     )
   }
 
-  # ---- gene-level ----
-  if (assay == "gene") {
-
-    gnx <- tximport::summarizeToGene(combined_prjs$lin_xpr$txi,
-                                     combined_prjs$lin_xpr$tx2gene)
-
-    rowRanges <- combined_prjs$gene_anno[combined_prjs$gene_anno$type == "gene", ]
-    rowRanges <- rowRanges[rowRanges$gene_id %in% rownames(gnx$counts), ]
-    rowRanges_order <- match(rownames(gnx$counts), mcols(rowRanges)$gene_id)
-
-    se <- SummarizedExperiment::SummarizedExperiment(
-      assays = list(
-        counts = gnx$counts,
-        abundance = gnx$abundance,
-        length = gnx$length
-      ),
-      colData = sample_metadata[, c("Raw reads",
-                                    "Clean reads",
-                                    "Dropped",
-                                    "Linearly unmapped",
-                                    "Linearly mapped")],
-      rowRanges = rowRanges[rowRanges_order, ]
-    )
+  ## check colnames already present in the SummarizedExperiment object
+  duplicated_cols <- which(colnames(se_coldata) %in% colnames(extra_coldata))
+  if (length(duplicated_cols > 0)) {
+    stop("coldata already has the following columns: ",
+         colnames(se_coldata)[duplicated_cols])
   }
 
-  # ---- transcript-level ----
-  if (assay == "trx") {
-    rowRanges <- combined_prjs$gene_anno[combined_prjs$gene_anno$type == "transcript", ]
-    rowRanges <- rowRanges[rowRanges$transcript_id %in% rownames(combined_prjs$lin_xpr$txi$counts), ]
-    rowRanges_order <- match(rownames(combined_prjs$lin_xpr$txi$counts),
-                             mcols(rowRanges)$transcript_id)
+  ## attach the extra columns
+  cbind(se_coldata, extra_coldata[rownames(se_coldata), ])
 
-    se <- SummarizedExperiment::SummarizedExperiment(
-      assays = list(
-        counts = combined_prjs$lin_xpr$txi$counts,
-        abundance = combined_prjs$lin_xpr$txi$abundance,
-        length = combined_prjs$lin_xpr$txi$length
-      ),
-      colData = sample_metadata[, c("Raw reads",
-                                    "Clean reads",
-                                    "Dropped",
-                                    "Linearly unmapped",
-                                    "Linearly mapped")],
-      # rowData = data.frame(combined_prjs$lin_xpr$tx2gene, row.names = "t_name"),
-      rowRanges = rowRanges[rowRanges_order, ]
-    )
-
-  }
-
-  se
 }
